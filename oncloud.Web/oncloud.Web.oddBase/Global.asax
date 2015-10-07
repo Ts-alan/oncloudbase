f7b64e46be04b722b0191d386990439daceb22fa
Anton Soloviev <solowk.tm+assembla@gmail.com>
2015-10-08 01:16:26 +0300

    initial import

---
+++ oncloud.Web/oncloud.Web.oddBase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="oncloud.Web.oddBase.MvcApplication" Language="C#" %>
---
+++ oncloud.Web/oncloud.Web.oddBase/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(oncloud.Web.oddBase.Startup))]
+namespace oncloud.Web.oddBase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
