67fda8c3dc97629d0c43d1527794be785d6d8f8d
denis <ts-alan@mail.ru>
2015-10-13 22:18:43 +0300

    Добавлен инкремент

---
+++ oncloud.oddBase/oncloud.Web.oddBase/Models/TableAdapterExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace oncloud.Web.oddBase.Models
+{
+    public class TableAdapterExtensions
+    {
+        public static int Increment()
+        {
+            int result;
+            int valueInrement;
+
+            using (EntitiesOddAdbase context = new EntitiesOddAdbase())
+            {
+
+                result = context.Increment.First().Counter;
+                valueInrement = result;
+
+                context.Increment.First().Counter = ++valueInrement;
+                context.SaveChanges();
+            }
+            return result;
+        }
+
+        public static string StringSymvol(string TypeConstruction = "BB")
+        {
+            string SymbolString = "";
+            int number = Increment();
+            for (int s = 8; s > number.ToString().Length; s--)
+            {
+                SymbolString = "0" + SymbolString;
+            }
+            var ValueString = TypeConstruction + SymbolString + number;
+            return ValueString;
+        }
+    }
+}
