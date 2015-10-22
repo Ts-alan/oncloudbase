dbcd9c4b969a010487813fd565e8afbec77578ff
Anton Soloviev <solowk.tm+assembla@gmail.com>
2015-10-22 09:55:04 +0300

    fix

--- oncloud.oddBase/oncloud.Domain/Concrete/EFDBContext.cs
+++ oncloud.oddBase/oncloud.Domain/Concrete/EFDBContext.cs
@@ -58,7 +58,5 @@ namespace oncloud.Domain.Concrete
         {
             Entry(entity).State = EntityState.Modified;
         }
-
-        public System.Data.Entity.DbSet<oncloud.Domain.Entities.TheHorizontalRoadMarking> TheHorizontalRoadMarkings { get; set; }
     }
 }
