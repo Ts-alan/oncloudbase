7eb0953b47dee5e684adb8394e1ca6aaf86bb5d8
denis <ts-alan@mail.ru>
2015-10-22 11:11:29 +0300

    Добавлен administationcontroller

---
+++ oncloud.oddBase/oncloud.Domain/Entities/SpecificationofRS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oncloud.Domain.Entities
+{
+     public  class SpecificationofRS
+    {
+        public int id { get; set; }
+        public int Street_id { get; set; }
+        public virtual Street Street { get; set; }
+    }
+}
