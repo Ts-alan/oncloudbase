a21024da7abc3dbf75feacc619c0061752c07876
Anton Soloviev <solowk.tm+assembla@gmail.com>
2015-11-03 10:13:59 +0300

    fixed meta chars

--- oncloud.oddBase/oncloud.Domain/Entities/IntelliSenseStreet.cs
+++ oncloud.oddBase/oncloud.Domain/Entities/IntelliSenseStreet.cs
@@ -6,26 +6,26 @@ using System.Data.Entity.Spatial;
 
 namespace oncloud.Domain.Entities
 {
-    [Table("IntelliSenseStreet")]
-    public partial class IntelliSenseStreet
-    {
-        [Key]
-        [Column(Order = 0)]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public int id { get; set; }
+	[Table("IntelliSenseStreet")]
+	public partial class IntelliSenseStreet
+	{
+		[Key]
+		[Column(Order = 0)]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int id { get; set; }
 
-        [Key]
-        [Column(Order = 1)]
-        [StringLength(50)]
-        [Display(Name="Улица")]
-        public string Street { get; set; }
+		[Key]
+		[Column(Order = 1)]
+		[StringLength(50)]
+		[Display(Name = "Улица")]
+		public string Street { get; set; }
 
-        [StringLength(50)]
-        [Display(Name = "Тип")]
-        public string Type { get; set; }
+		[StringLength(50)]
+		[Display(Name = "Тип")]
+		public string Type { get; set; }
 
-        public int CityId { get; set; }
-        [Display(Name = "Город")]
-        public virtual City City { get; set; }
-    }
+		public int CityId { get; set; }
+		[Display(Name = "Город")]
+		public virtual City City { get; set; }
+	}
 }
