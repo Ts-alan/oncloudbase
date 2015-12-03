dbb72f721a37c49827ad5923ee3db22445a0ad2c
denis <ts-alan@mail.ru>
2015-12-03 20:50:13 +0200

    Готовые слайдеры для всего

--- oncloud.oddBase/oncloud.Web.oddBase/Controllers/HomeController.cs
+++ oncloud.oddBase/oncloud.Web.oddBase/Controllers/HomeController.cs
@@ -392,8 +392,7 @@ namespace oncloud.Web.oddBase.Controllers
 
         public ActionResult Review(int id)
         {
-            ViewBag.RoadSigns = db.RoadSigns.ToList();
-            ViewBag.RoadBarriers = db.RoadBarriers.ToList();
+         
             Street street = db.Street.Find(id);
             return View(street);
         }
